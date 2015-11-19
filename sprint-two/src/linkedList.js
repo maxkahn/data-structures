45aa4a3194177939b7902becd766a0ae3b9fd2b1
hridhya <menonhridhya@gmail.com>
2015-11-18 22:48:30 -0800

    Implemented linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = new Node(value);
+    var temp = list.head;
+    if (list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    }
+    else {
+      while(temp.next !== null) {
+        temp = temp.next;
+      }
+      temp.next = newTail;
+      list.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
+
+    if (list.head !== null) {
+      
+      var removedHead = list.head;
+      delete list[list.head.value];
+      list.head = list.head.next ? list.head.next : null;
+      return removedHead.value;
+    }
+      return null;
   };
 
   list.contains = function(target) {
+
+    var temp = list.head;
+    while(temp !== null){
+      if (temp.value === target){
+        return true;
+      }
+      else{
+        temp = temp.next;
+      }
+    }
+    return false;
   };
 
   return list;
