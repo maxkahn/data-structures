d5cd0c6d188cfd422342c67978b555cc80766ac3
maxkahn <kahn.max@gmail.com>
2015-11-18 16:20:41 -0800

    Implemented stack.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var instance = {};
+instance.stackSize = 0;
+instance.storage = {};
+
+_.extend(instance, stackMethods);
+
+return instance;
+
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.stackSize++] = value;
+
+};
+
+stackMethods.pop = function() {
+  var popped = this.storage[this.stackSize - 1];
+  if (this.stackSize > 0) {
+      this.stackSize--;
+    }
+  return popped;
+};
+
+stackMethods.size = function() {
+  //var size = this.size;
+  return this.stackSize;
+
+};
+
+
+
 
 
