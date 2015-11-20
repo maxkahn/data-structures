6e42f881847afe3914ebeb13bababccd0812a031
maxkahn <kahn.max@gmail.com>
2015-11-20 11:28:06 -0800

    All unit tests pass.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,34 +3,29 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this._keys = [];
-  this._indices = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (_.contains(this._keys, k)) {
-    this._storage.set(index, v);
-    //this._storage[index] = v;
+
+  if (this._storage.get(index) !== undefined) {
+    var bucket = this._storage.get(index);
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket[i][1] = v;
+        this._storage.set(index, bucket);
+        return;
+      }
+    }
+    bucket[bucket.length] = [k, v];
+    this._storage.set(index, bucket);
   }
-  /*else if (_.contains(this._indices, index)){
-    this._storage = Array.prototype.concat.call(this._storage, LimitedArray(this._limit));
-    //this._storage = this._storage.LimitedArray(this._limit));
-    this._storage[index * 2 + 1] = v;
-    this._keys.push(k);
-    this._indices.push(index *2 + 1);
-  }*/
+
   else {
-    this._keys.push(k);
-    this._indices.push(index);
-    this._storage.set(index, v);
-    //this._storage[index] = v;
+    var bucket = [];
+    bucket[0]=[k,v];
+    this._storage.set(index, bucket);
   }
-  /*if(this._storage.hasOwnProperty(index)){
-    this._limit = 2 * this._limit;
-    this._storage = this._storage.concat(LimitedArray(this._limit));
-    this.insert(k, v);
-  }*/
 
     
   
@@ -39,18 +34,23 @@ HashTable.prototype.insert = function(k, v) {
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      var val = bucket[i][1];
+      return val;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(val, i, arr) {
-    if (i === index) {
-      delete arr[i];
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
     }
-  });
-  //delete this._storage[index];
-  delete this._keys[k];
+  }
 };
 
 
