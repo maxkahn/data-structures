d2f3b807c95a4bb9d3cbb93e78e499b1e3889f8d
maxkahn <kahn.max@gmail.com>
2015-11-19 18:14:18 -0800

    Implemented few more

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,41 @@
 var BinarySearchTree = function(value) {
+  var instance = {};
+  instance.left = null;
+  instance.right = null;
+  instance.value = value;
+  _.extend(instance, binarysearchmethods);
+  return instance;
 };
 
+var binarysearchmethods = {};
 
+binarysearchmethods.insert = function(value) {
+  if(value < this.value) {
+    if (this.left !== null) {
+      this.left.insert(value);
+    }
+    else {
+      this.left = BinarySearchTree(value);
+    }
+  }
+  else {
+    if (this.right !== null) {
+      this.right.insert(value);
+    }
+    else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+};
+
+binarysearchmethods.contains = function(value) {
+  
+};
+
+binarysearchmethods.depthFirstLog = function(callback) {};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//var binarySearchTree = new BinarySearchTree();
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,54 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._keys = [];
+  this._indices = [];
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (_.contains(this._keys, k)) {
+    this._storage.set(index, v);
+    //this._storage[index] = v;
+  }
+  /*else if (_.contains(this._indices, index)){
+    this._storage = Array.prototype.concat.call(this._storage, LimitedArray(this._limit));
+    //this._storage = this._storage.LimitedArray(this._limit));
+    this._storage[index * 2 + 1] = v;
+    this._keys.push(k);
+    this._indices.push(index *2 + 1);
+  }*/
+  else {
+    this._keys.push(k);
+    this._indices.push(index);
+    this._storage.set(index, v);
+    //this._storage[index] = v;
+  }
+  /*if(this._storage.hasOwnProperty(index)){
+    this._limit = 2 * this._limit;
+    this._storage = this._storage.concat(LimitedArray(this._limit));
+    this.insert(k, v);
+  }*/
+
+    
+  
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(val, i, arr) {
+    if (i === index) {
+      delete arr[i];
+    }
+  });
+  //delete this._storage[index];
+  delete this._keys[k];
 };
 
 
