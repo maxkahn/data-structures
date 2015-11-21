c20e9764f65b95fe93c6f1a051239c85ce4323f0
maxkahn <kahn.max@gmail.com>
2015-11-20 22:08:34 -0800

    Final changes Friday night before leaving.

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -8,14 +8,41 @@ var doublyLinkedList = function() {
     //set previous of newTail to be the current tail
     //set next to be null
     var newTail = Node(value);
-    var 
+    var temp = list.tail;
+    if(list.head === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    }
+    else {
+      temp.next = newTail;
+      newTail.previous = temp;
+      list.tail = newTail;
+
+    }
   };
 
   list.removeHead = function(value) {
     //find what will be the nextHead
     //update the head property on our DLL
     //set the next property on our nextHead to be the past head
+    //set 
+    var temp = list.head;
+    delete list.head;
+    list.head = temp.next;
 
+  };
+
+  list.contains = function(value) {
+    var temp = list.head;
+    while(temp !== null) {
+      if (temp.value === value) {
+        return true;
+      }
+      else {
+        temp = temp.next;
+      }
+    }
+    return false;
   };
 
   return list; 
